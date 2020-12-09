0a9b59c5267939f9846eb7d8799d2955375c4101
Eric Della <ericdella13@gmail.com>
2020-12-08 17:10:52 -0800

    added localstorage, dark mode hook, added dark mode hook into app

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+import useLocalStorage from "./useLocalStorage"
+
+
+const useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage(initialValue);
+    return [darkMode, setDarkMode]
+
+
+}
+
+export default useDarkMode
